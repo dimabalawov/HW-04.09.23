f2ec1e31904ee213bffec507ec1a9a4b5d94c3fc
dimabalawov <dimabalawov@gmail.com>
2023-09-04 22:13:08 +0300

    Удаление, добавление и просмотр сотрудников

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -9,8 +10,7 @@ struct Employee {
     char phonenumber[20];
     double salary;
 };
-unsigned int amountofemplo=0;
-Employee* emplo = new Employee[amountofemplo];
+int amountofemplo=0;
 void readFile(const char* path)
 {
     FILE* file = fopen(path, "r");
@@ -21,29 +21,49 @@ void readFile(const char* path)
     fclose(file);
 }
 
-void addEmployee(const char* path)
+void addEmployee(const char* path,Employee* &arr)
 {
+    Employee* newarr = new Employee[amountofemplo+1];
+    for (size_t i = 0; i < amountofemplo; i++)
+    {
+        newarr[i] = arr[i];
+    }
     cout << "Enter the first name of the employee: ";
-    cin >> emplo[amountofemplo].firstname;
+    cin >> newarr[amountofemplo].firstname;
     cout << "Enter the last name of the employee: ";
-    cin >> emplo[amountofemplo].lastname;
+    cin >> newarr[amountofemplo].lastname;
     cout << "Enter the phone number of the employee: ";
-    cin >> emplo[amountofemplo].phonenumber;
+    cin >> newarr[amountofemplo].phonenumber;
     cout << "Enter the salary of the employee: ";
-    cin >> emplo[amountofemplo].salary;
+    cin >> newarr[amountofemplo].salary;
     FILE* file = fopen(path, "a");
-    fprintf_s(file, "FIRST NAME: %s\n", emplo[amountofemplo].firstname);
-    fprintf_s(file, "LAST NAME: %s\n", emplo[amountofemplo].lastname);
-    fprintf_s(file, "PHONE NUMBER: %s\n", emplo[amountofemplo].phonenumber);
-    fprintf_s(file, "SALARY: %f\n", emplo[amountofemplo].salary);
-    amountofemplo++;
+    fprintf_s(file, "FIRST NAME: %s\n", newarr[amountofemplo].firstname);
+    fprintf_s(file, "LAST NAME: %s\n", newarr[amountofemplo].lastname);
+    fprintf_s(file, "PHONE NUMBER: %s\n", newarr[amountofemplo].phonenumber);
+    fprintf_s(file, "SALARY: %f\n", newarr[amountofemplo].salary);
     fprintf_s(file, "---\n");
     fclose(file);
+    amountofemplo++;
+    delete[] arr;
+    arr = newarr;
 }
 
-void deleteEmployee(const char* path, Employee* arr)
+void rewriteFile(const char* path, Employee*& arr)
 {
-    Employee* newarr = new Employee;
+    FILE* file = fopen(path, "w");
+    for (size_t i = 0; i < amountofemplo; i++)
+    {
+        fprintf_s(file, "FIRST NAME: %s\n", arr[i].firstname);
+        fprintf_s(file, "LAST NAME: %s\n", arr[i].lastname);
+        fprintf_s(file, "PHONE NUMBER: %s\n", arr[i].phonenumber);
+        fprintf_s(file, "SALARY: %f\n", arr[i].salary);
+        fprintf_s(file, "---\n");
+    }
+    fclose(file);
+}
+void deleteEmployee(const char* path, Employee* &arr)
+{
+    Employee* newarr = new Employee[amountofemplo-1];
     char firstname[20];
     char lastname[20];
     cout << "Enter the first name of the employee: ";
@@ -53,7 +73,7 @@ void deleteEmployee(const char* path, Employee* arr)
     int index=0;
     for (int i = 0; i < amountofemplo; i++)
     {
-        if (firstname == arr[i].firstname && lastname == arr[i].lastname)
+        if (!(strcmp(firstname,arr[i].firstname)) && !(strcmp(lastname, arr[i].lastname)))
         {
             index = i;
             for (size_t j = 0; j < amountofemplo; j++)
@@ -61,27 +81,28 @@ void deleteEmployee(const char* path, Employee* arr)
                 if (j != index)
                     newarr[j] = arr[j];
             }
+            amountofemplo--;
+            delete[] arr;
+            arr = newarr;
+            rewriteFile(path, arr);
         }
+        else if(i== amountofemplo-1)
+            cout << "No such employee has found" << endl;
+
     }
-    amountofemplo--;
-    delete[] emplo;
-    for (size_t i = 0; i < amountofemplo; i++)
-    {
-        emplo[i] = newarr[i];
-    }
-    delete[] newarr;
-    
 }
 
 int main()
 {
+    Employee* emplo = new Employee[amountofemplo];
     const int SIZE = 100;
     char path[SIZE];
     unsigned short choice = 1;
     cout << "Type the path to the file: " << endl;
     cin >> path;
-
-    addEmployee(path);
-    addEmployee(path);
+    addEmployee(path, emplo);
+    addEmployee(path, emplo);
+    readFile(path);
+    deleteEmployee(path, emplo);
     readFile(path);
 }
